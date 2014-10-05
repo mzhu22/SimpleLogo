5ba751dd03c9b6630aaaf79cb6004e8b75ba92f1
safkat23 <si35@duke.edu>
2014-10-05 00:04:12 -0400

    Deleting and creating classes for revised api.

--- src/frontend/GUIFeature.java
+++ src/frontend/GUIFeature.java
@@ -7,12 +7,25 @@ public abstract class GUIFeature {
 	protected double myX;
 	protected double myY;
 	
+	/**
+	 * Constructor.
+	 * @param x The x-coordinate of the tool on the screen.
+	 * @param y The y-coordinate of the tool on the screen.
+	 */
 	public GUIFeature(double x, double y){
 		myX = x;
 		myY = y;
 	}
 	
+	/**
+	 * This method represents the action taken by the feature when it is interacted upon.
+	 * For example, the turtle changes color if the changeColor button is pressed.
+	 */
 	public abstract void action();
 	
+	/**
+	 * Creates the graphical representation of the GUIFeature.
+	 * @return The JavaFX Node object that is the graphical representation and that the user interacts with.
+	 */
 	public abstract Node makeTool();
 }

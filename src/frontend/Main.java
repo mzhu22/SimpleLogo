8239d926c50e444d66ab6ec0d9a1c16d485f8352
Chris <ChrisToast.users.noreply.github.com>
2014-10-06 22:12:14 -0400

    Merged basic GUI with api hierarchy

---
+++ src/frontend/Main.java
@@ -0,0 +1,19 @@
+package frontend;
+
+
+/**
+ * @author Chris Bernt
+ * Class whose sole purpose is to start the program.
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     * Runs the program.
+     */
+    public static void main(final String[] args) {
+        final Initializer init = new Initializer();
+        init.begin(args);
+    }
+}

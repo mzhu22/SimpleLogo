d3246862bd06aeb7b3ea549eb59f7f435221e7f7
safkat23 <si35@duke.edu>
2014-10-19 23:14:13 -0400

    Added help page functionality.

--- src/frontend/HelpButton.java
+++ src/frontend/HelpButton.java
@@ -1,14 +1,40 @@
 package frontend;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+
 public class HelpButton extends GUIFeatureWithButton {
 
+	private final static int WIDTH = 800;
+	private final static int HEIGHT = 600;
+	private final static String DEFAULT_ADDRESS = "http://www.cs.duke.edu/courses/compsci308/current/assign/03_slogo/commands.php";
+	private final static String TITLE = "Help Page";
+	private String myWebAddress;
+	
+	public HelpButton(double x, double y, String buttonName, String address)
+	{
+		super(x,y, buttonName);
+		this.myWebAddress = address;
+		
+	}
 	public HelpButton(double x, double y, String buttonName) {
-		super(x, y, buttonName);
+		this(x, y, buttonName, DEFAULT_ADDRESS);
 	}
 
 	@Override
 	public void action() {
-		System.out.println("Help.");
+		WebView browser = new WebView();
+		WebEngine webEngine = browser.getEngine();
+		webEngine.load(this.myWebAddress);
+		Group g = new Group();
+		g.getChildren().add(browser);
+		Stage s = new Stage();
+		s.setTitle(TITLE);
+		s.setScene(new Scene(g, WIDTH, HEIGHT));
+		s.show();
 	}
 
 }

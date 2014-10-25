bde6755cc93232227aef1286be0e8b4ace982ed7
safkat23 <si35@duke.edu>
2014-10-25 01:41:26 -0400

    Added history functionality.

--- src/frontend/ConcreteFeatures/InputTextBox.java
+++ src/frontend/ConcreteFeatures/InputTextBox.java
@@ -3,6 +3,7 @@ package frontend.ConcreteFeatures;
 import javafx.scene.control.TextArea;
 import SLogoControllers.InputController;
 import errorsAndExceptions.ErrorPopUp;
+import expressionTree.HistoryCollection;
 import expressionTree.VariableNodeMap;
 import frontend.PaneUpdater;
 import frontend.Translator;
@@ -14,13 +15,15 @@ public class InputTextBox extends GUIFeatureWithTextBox {
 	private TurtleMover myMover;
 	private PaneUpdater myPaneUpdater;
 	private Translator myTranslator;
+	private HistoryCollection myHistory;
 	
-	public InputTextBox(double x, double y, double width, double height, TurtleMover mover, String buttonName, String initialText, PaneUpdater pu, Translator t) {
+	public InputTextBox(double x, double y, double width, double height, TurtleMover mover, String buttonName, String initialText, PaneUpdater pu, Translator t, HistoryCollection history) {
 		super(x, y, width, height, buttonName, initialText);
 		this.inputBox = new TextArea();
 		this.myMover = mover;
 		this.myPaneUpdater = pu;
 		this.myTranslator = t;
+		this.myHistory = history;
 	}
 
 	@Override
@@ -29,6 +32,7 @@ public class InputTextBox extends GUIFeatureWithTextBox {
 		
 		if(input != null && !input.isEmpty()){
 			InputController ic = new InputController(myTranslator);
+			myHistory.add(input);
 			ic.giveInput(input);
 			this.myMover.startDrawing(ic.getInstructions());
 			this.myPaneUpdater.updateAll();

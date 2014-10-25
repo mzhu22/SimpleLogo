c58ffa933ed70be8d22cf22ba69d180a71e3a1d3
Mike Zhu <mzhu22@gmail.com>
2014-10-24 23:29:39 -0400

    Reflection appears to work

--- src/ExpressionTree/ExpressionNodeFactory.java
+++ src/ExpressionTree/ExpressionNodeFactory.java
@@ -3,43 +3,20 @@ package ExpressionTree;
 import java.util.Stack;
 
 import CommandAbstractClasses.Constant;
-import Comparators.And;
-import Comparators.Equal;
-import Comparators.Greater;
-import Comparators.Less;
-import Comparators.Not;
-import Comparators.NotEqual;
-import Comparators.Or;
-import MathOperations.Add;
-import MathOperations.Arctan;
-import MathOperations.Cosine;
-import MathOperations.Difference;
-import MathOperations.Log;
-import MathOperations.Minus;
-import MathOperations.Power;
-import MathOperations.Product;
-import MathOperations.Quotient;
-import MathOperations.Randoms;
-import MathOperations.Remainder;
-import MathOperations.Sine;
-import MathOperations.Tangent;
-import controlStructures.Make;
-import controlStructures.Repeat;
-import controlStructures.To;
-import displayCommands.Back;
-import displayCommands.Forward;
-import displayCommands.HideTurtle;
-import displayCommands.Left;
-import displayCommands.PenDown;
-import displayCommands.PenUp;
-import displayCommands.Right;
-import displayCommands.ShowTurtle;
 
 public final class ExpressionNodeFactory {
 
 	private VariableNodeMap myVariables = VariableNodeMap.getVariableNodeMap();
 	private UserFunctionNodeMap myUserFunctions = UserFunctionNodeMap.getUserFunctionNodeMap();
 		
+	private String[] myPackages = { "CommandAbstractClasses", 
+								    "Comparators",
+									"controlStructures",
+									"displayCommands",
+									"ExpressionTree",
+									"MathOperations",
+									"TurtleQueries"};
+	
 	
 	public  Stack<ExpressionNode> getAllNodes(String s){
 		int balance = 0; 
@@ -85,7 +62,13 @@ public final class ExpressionNodeFactory {
 		}
 		catch(NumberFormatException e ){	
 			
-			return commandHandler(s);
+			try {
+				return commandHandler(s);
+			} catch (InstantiationException e1) {
+				e1.printStackTrace();
+			} catch (IllegalAccessException e1) {
+				e1.printStackTrace();
+			}
 		}
 
 		toReturn = new Constant(); 
@@ -94,74 +77,20 @@ public final class ExpressionNodeFactory {
 
 	}
 
-	private ExpressionNode commandHandler(String s) {
-		switch (s) {
-			//Display Node
-			case "FORWARD" : case "FD": return new Forward();
+	private ExpressionNode commandHandler(String command) throws InstantiationException, IllegalAccessException{
+		ExpressionNode node = new doNothing();
+		
+		for(String packageName : myPackages){
+			String className = packageName + "." + command;
 			
-			case "RIGHT" : case "RT" : return new Right();
-			
-			case "PENUP" : case "PU": return new PenUp();
-			
-			case "PENDOWN" : case "PD": return new PenDown();
-			
-			case "BACK" : case "BK" : return new Back();
-			
-			case "LEFT" : case "LT" : return new Left();
-			
-			case "SHOWTURTLE" : case "ST" : return new ShowTurtle();
-			
-			case "HIDETURTLE" : case "HT" : return new HideTurtle();
-			
-			case "SUM" : case  "+": return new Add(); 
-
-			case "DIFFERENCE" : case "-": return new Difference(); 
-
-			case "PRODUCT" : case "*" : return new Product(); 
-
-			case "QUOTIENT" : case "/" : return new Quotient(); 
-
-			case "REMAINDER" : case "%" : return new Remainder(); 
-
-			case "MINUS" : case "~": return new Minus(); 
-
-			case "RANDOM" : return new Randoms();
-
-			case "SIN" : return new Sine(); 
-
-			case "COS" : return new Cosine(); 
-
-			case "TAN" : return new Tangent(); 
-
-			case "ATAN" : return new Arctan(); 
-
-			case "LOG" : return new Log(); 
-
-			case "POW" : return new Power(); 
-
-			case "AND" : return new And(); 
-
-			case "OR" : return new Or(); 
-
-			case "NOT" : return new Not(); 
-
-			case "NOTEQUAL?" : case "NOTEQUALP" :return new NotEqual(); 
-
-			case "EQUAL?" : case "EQUALP" :return new Equal(); 
-
-			case "GREATER?" : case "GREATERP" :return new Greater(); 
-
-			case "LESS?" : case  "LESSP" : return new Less(); 
-			
-			case "REPEAT" : return new Repeat();
-
-			case "MAKE" : return new Make(); 
-			
-			case "TO" : return new To();
-						
-			default: return unknownFunctionHandler(s); 
-
+			try {
+				Class<?> c = Class.forName(className);
+				node = (ExpressionNode) c.newInstance();
+			} catch (ClassNotFoundException e) {
+				continue;
+			}
 		}
+		return node;
 	}
 
 	public ExpressionNode variableHandler(String s){
@@ -188,4 +117,8 @@ public final class ExpressionNodeFactory {
 		return new doNothing(s);
 	}
 
+	public static void main(String[] args) throws InstantiationException, IllegalAccessException{
+		ExpressionNodeFactory temp = new ExpressionNodeFactory();
+		System.out.println(temp.commandHandler("Forward"));
+	}
 }

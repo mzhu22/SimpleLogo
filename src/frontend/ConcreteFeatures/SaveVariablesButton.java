e2c24bd33a79969de8eccce9f60cfeea0b69c424
safkat23 <si35@duke.edu>
2014-10-25 20:48:50 -0400

    Added load and save variables buttons.

---
+++ src/frontend/ConcreteFeatures/SaveVariablesButton.java
@@ -0,0 +1,18 @@
+package frontend.ConcreteFeatures;
+
+import frontend.AbstractFeatures.GUIFeatureWithButton;
+
+public class SaveVariablesButton extends GUIFeatureWithButton {
+
+	public SaveVariablesButton(double x, double y, String buttonName) {
+		super(x, y, buttonName);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void action() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
